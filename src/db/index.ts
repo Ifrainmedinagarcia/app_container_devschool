875b09d33a8c7e53238b04374ceee065b8708a48
Ifrain <imedinag@achs.cl>
2022-09-19 18:54:51 -0300

    fix routes

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,12 +6,12 @@ export const options: Options[] = [
   {
     name: "ir a la tienda",
     img: "https://img.freepik.com/foto-gratis/familia-centro-comercial_1098-15029.jpg?w=2000&t=st=1663075901~exp=1663076501~hmac=bbbf6aee60a0c5ae7b9ef74dac9873bc7fb45da1e0ee85af3d14d858020e6769",
-    to: "/man"
+    to: "/app_container_devschool/man"
   },
 
   {
     name: "Hazte socio",
     img: "https://img.freepik.com/foto-gratis/concepto-hombres-negocios-apreton-manos_53876-31214.jpg?w=2000&t=st=1663076004~exp=1663076604~hmac=e5316d6ffc1d40afcc2d2d7e4580a6cedf7d2c071aff95ee3ae50225c99d483b",
-    to: "/become-partner"
+    to: "/app_container_devschool/become-partner"
   },
 ]
